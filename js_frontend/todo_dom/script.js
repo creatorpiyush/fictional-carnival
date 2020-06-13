6ad619ecd7edf17711bda81db5e5539e3b108231
Creator Piyush <piyushanand.1221@gmail.com>
2020-06-13 18:10:00 +0530

    TODO list using DOM functions

--- js_frontend/todo_dom/script.js
+++ js_frontend/todo_dom/script.js
@@ -6,11 +6,24 @@ addbtn.onclick = function () {
   let li = document.createElement("li");
   li.innerText = task.value;
 
+  // * delete button
   let Xbtn = document.createElement("button");
   Xbtn.innerText = "❌";
   Xbtn.onclick = (event) => {
     event.target.parentElement.remove();
   };
+
+  // * order shift
+  let Upbtn = document.createElement("button");
+  Upbtn.innerText = "⬆";
+  Upbtn.onclick = (event) => {
+    event.target.parentElement.parentElement.insertBefore(
+      event.target.parentElement,
+      event.target.parentElement.previousSibling
+    );
+  };
+
   li.appendChild(Xbtn);
+  li.appendChild(Upbtn);
   list.appendChild(li);
 };
