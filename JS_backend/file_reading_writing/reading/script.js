199ac90994495e47003b91da0c0d887ef5fa8e2b
creatorpiyush <piyushanand.1221@gmail.com>
2020-06-29 17:15:04 +0530

    Reading a file in js

--- JS_backend/file_reading_writing/reading/script.js
+++ JS_backend/file_reading_writing/reading/script.js
@@ -1,9 +1,20 @@
 const fs = require(`fs`);
 
+// * reading from different folder
 fs.readFile("../writing/sample.txt", (err, data) => {
   if (err) throw err;
 
   console.log(data); // * output: content of file in buffer form
 
   console.log(data.toString()); // * output: content in string form
+});
+
+// * reading from same folder
+
+fs.readFile(__dirname + `/write.txt`, (err, data) => {
+  if (err) throw err;
+
+  console.log(data);
+
+  console.log(data.toString());
 });
