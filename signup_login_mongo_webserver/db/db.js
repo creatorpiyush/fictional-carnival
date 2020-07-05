6198a8ac2da57453d4c9fa11b236eebac1999f38
creatorpiyush <piyushanand.1221@gmail.com>
2020-07-06 00:46:42 +0530

    database linking with mongo_web_server

---
+++ signup_login_mongo_webserver/db/db.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const db_url = `mongodb://localhost:27017/loginsignup_db`;
+
+// connection created
+mongoose.connect(
+  db_url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err, info) => {
+    if (err) console.log(err);
+    else {
+      console.log(`DB connect Sucess...`);
+      console.log(info);
+    }
+  }
+);
+
+// userSchema created
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+mongoose.model("users", userSchema);
+
+module.exports = mongoose.model("users");
