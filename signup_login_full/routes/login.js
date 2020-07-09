ce2476ca20a64f16bdc6c3f0b42fe8c69d2e24ac
creatorpiyush <piyushanand.1221@gmail.com>
2020-07-10 00:18:35 +0530

    express-session added

--- signup_login_full/routes/login.js
+++ signup_login_full/routes/login.js
@@ -1,13 +1,22 @@
 const route = require("express").Router();
 const bcrypt = require("bcryptjs");
+const session = require("express-session");
 
-const { check, validationResult, Result } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../db/user");
 
 route.get("/", (req, res) => {
   res.status(200).render("login");
 });
+
+route.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: `U{]k!Zzz/3wxvJK)d$qp@>Bz#`,
+  })
+);
 
 route.post(
   "/dXNlcmxvZ2lu",
@@ -18,19 +27,23 @@ route.post(
   (req, res) => {
     const err = validationResult(req);
     if (!err.isEmpty())
-      return res
-        .status(500)
-        .render("login", {
-          error: `${err.errors[0].msg} for ${err.errors[0].param} `,
-        });
+      return res.status(500).render("login", {
+        error: `${err.errors[0].msg} for ${err.errors[0].param} `,
+      });
 
+    // * comparing password
     User.findOne({ email: req.body.email }, (err, result) => {
       if (err) return res.status(422).render("login", { error: err });
 
       if (result) {
         const isMatch = bcrypt.compareSync(req.body.password, result.password);
-        if (isMatch) return res.status(201).redirect("/");
-        else {
+        if (isMatch) {
+          // session connected to user
+          req.session.userId = result.id;
+          console.log(req.session.userId);
+
+          return res.status(201).redirect("/YXBp/cHJvZmlsZQ==");
+        } else {
           return res
             .status(421)
             .render("login", { error: `Password not matched` });
--- signup_login_full/routes/profile.js
+++ signup_login_full/routes/profile.js
@@ -1,7 +1,16 @@
 const route = require("express").Router();
 
-const User = route.get("/", (req, res) => {
+const User = require("../db/user");
+
+route.get("/", (req, res) => {
+  if (!req.session.userId) {
+    return res.render(`login`);
+  }
+
+  const user = User.findById(req.session.userId);
   res.status(200).render("profile", { user });
 });
 
 // todo: apply session in connection
+
+module.exports = route;
