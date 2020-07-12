f898a93f7696a28b09d3ce58c73fd5ebe1053212
creatorpiyush <piyushanand.1221@gmail.com>
2020-07-12 12:59:44 +0530

    some session values added

--- express_session_cookies/server.js
+++ express_session_cookies/server.js
@@ -6,15 +6,21 @@ const session = require("express-session");
 
 const port = process.env.PORT || 4444;
 
-app.use(session({ secret: `Bg,ve:;x3#bFykc+.^dk2E=y` }));
+app.use(
+  session({
+    secret: `Bg,ve:;x3#bFykc+.^dk2E=y`,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.get("/session_test", (req, res) => {
   if (req.session.count) {
     req.session.count++;
-    res.send(`Count: ${req.session.count}`);
+    res.send(`Count: ${req.session.id}`);
   } else {
     req.session.count = 1;
-    res.send(`Welcome First Time: ${req.session.count}`);
+    res.send(`Welcome First Time: ${req.session.id}`);
   }
 });
 
