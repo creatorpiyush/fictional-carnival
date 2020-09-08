752ed85584b5edcab8562ab666912770d2d53906
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-09 00:43:30 +0530

    removing some comments

--- passport_demo_google/routes/good.js
+++ passport_demo_google/routes/good.js
@@ -1,8 +1,6 @@
 const route = require("express").Router();
 
 route.get("/", (req, res) => {
-  //   res.send(req.user.emails[0].value);
-  //   res.send(req.user)
   res.send(
     `Welcome mr ${req.user.username}! <br> Your email-id : ${req.user.email} `
   );
