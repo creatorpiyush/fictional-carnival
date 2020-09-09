adde900ef77791921c386b2f63bbc6a3d76697d6
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-09 11:28:48 +0530

    facebook auth added

--- passport_demo_google/routes/passport_setup.js
+++ passport_demo_google/routes/passport_setup.js
@@ -120,25 +120,25 @@ passport.use(
       console.log(profile);
       return done(null, profile);
 
-      // User.findOne({ facebookid: profile.id }).then((currentUser) => {
-      //   if (currentUser) {
-      //     //! already have user
-      //     console.log("User is:", currentUser);
-      //     done(null, currentUser);
-      //   } else {
-      //     // * create user in db
-      //     new User({
-      //       username: profile.displayName,
-      //       facebookid: profile.id,
-      //       email: profile.emails[0].value,
-      //     })
-      //       .save()
-      //       .then((newUser) => {
-      //         console.log("new user created" + newUser);
-      //         done(null, newUser);
-      //       });
-      //   }
-      // });
+      User.findOne({ facebookid: profile.id }).then((currentUser) => {
+        if (currentUser) {
+          //! already have user
+          console.log("User is:", currentUser);
+          done(null, currentUser);
+        } else {
+          // * create user in db
+          new User({
+            username: profile.displayName,
+            facebookid: profile.id,
+            email: profile.emails[0].value,
+          })
+            .save()
+            .then((newUser) => {
+              console.log("new user created" + newUser);
+              done(null, newUser);
+            });
+        }
+      });
     }
   )
 );
