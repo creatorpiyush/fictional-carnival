cd91ba2ea616caab91483518f5c7db463dc07090
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-10 11:31:49 +0530

    session handelling left

--- login_Profile_with_db/routes/index.js
+++ login_Profile_with_db/routes/index.js
@@ -5,13 +5,10 @@ const User = require("../db/db");
 route.get("/", (req, res) => {
   // console.log(req.session);
   // res.send(req.session);
+  if (req.session.passport == undefined || req.session.passport.user == null) {
+    return res.redirect("/login");
+  }
 
-  if (req.session.passport == undefined) {
-    return res.redirect("/login");
-  }
-  if (req.session.userId == undefined) {
-    return res.redirect("/login");
-  }
   User.findById(req.session.userId || req.session.passport.user).then(
     (user) => {
       res.render("index", { username: user.username, email: user.email });
