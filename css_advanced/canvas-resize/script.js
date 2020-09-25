b178da406488590662cff6144ed0c13a3342e22c
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-25 21:11:38 +0530

    fillRect in canvas

--- css_advanced/canvas-resize/script.js
+++ css_advanced/canvas-resize/script.js
@@ -4,3 +4,12 @@ const canvas = document.querySelector("canvas");
 // * canvas has some margin by default so we need remove that through css
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let c = canvas.getContext("2d");
+// console.log(c);
+
+// ? fillRect create rectangle on screen
+// c.fillRect(x, y, width, height);
+c.fillRect(100, 100, 100, 100);
+c.fillRect(400, 100, 100, 100);
+c.fillRect(300, 300, 100, 100);
