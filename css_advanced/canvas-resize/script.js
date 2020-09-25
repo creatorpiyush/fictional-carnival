1578a3f3fc5431c25aec3712f00d1308643f429d
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-25 21:00:19 +0530

    setting width and height in canvas

---
+++ css_advanced/canvas-resize/script.js
@@ -0,0 +1,6 @@
+const canvas = document.querySelector("canvas");
+
+// ? adding height and width through js
+// * canvas has some margin by default so we need remove that through css
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
