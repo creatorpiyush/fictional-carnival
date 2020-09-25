1375848874c81c99883e0e78006bc938d07d572a
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-25 23:30:54 +0530

    arc / circles in canvas

--- css_advanced/canvas_resize_and_object/script.js
+++ css_advanced/canvas_resize_and_object/script.js
@@ -10,8 +10,11 @@ let c = canvas.getContext("2d");
 
 // ? fillRect create rectangle on screen
 // c.fillRect(x, y, width, height);
+c.fillStyle = "red";
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = "blue";
 c.fillRect(400, 100, 100, 100);
+c.fillStyle = "green";
 c.fillRect(300, 300, 100, 100);
 
 // ? Lines
@@ -20,4 +23,23 @@ c.beginPath();
 c.moveTo(50, 300);
 // c.lineTo(x,y)
 c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "blue";
 c.stroke();
+
+// ? Arc / Circle
+
+// c.arc(x, y, r, startAngle, endAngle, drawCounterClockwise(boolean));
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = "#fa34a3";
+// c.stroke();
+
+for (let i = 0; i < 3; i++) {
+  let x = Math.random() * window.innerWidth;
+  let y = Math.random() * window.innerHeight;
+  c.beginPath();
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
+  c.strokeStyle = "#fa34a3";
+  c.stroke();
+}
