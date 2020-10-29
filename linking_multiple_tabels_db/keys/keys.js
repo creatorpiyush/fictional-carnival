26a60410cd73af2b66c9902530d915516b2ee7e5
Piyush Anand <piyushanand.1221@gmail.com>
2020-10-30 01:40:45 +0530

    keys changed

--- linking_multiple_tabels_db/keys/keys.js
+++ linking_multiple_tabels_db/keys/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoose: {
-    db_url: `mongodb+srv://multiple:multiple@cluster0.7mqzk.mongodb.net/linking_multiple_db?retryWrites=true&w=majority`,
+    db_url: `mongodb://localhost:27017/testing_2_tables`,
   },
 };
