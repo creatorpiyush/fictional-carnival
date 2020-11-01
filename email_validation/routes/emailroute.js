91ad663884591ada3d5f1dec152fbeed339bab27
Piyush Anand <piyushanand.1221@gmail.com>
2020-11-02 01:43:44 +0530

    confirmation authintication added

--- email_validation/routes/emailroute.js
+++ email_validation/routes/emailroute.js
@@ -1,5 +1,7 @@
 const route = require("express").Router();
 
+const User = require("../model.js");
+
 let userid = "";
 
 route.get("/", (req, res) => {
@@ -8,7 +10,17 @@ route.get("/", (req, res) => {
 
 route.get("/:id", (req, res) => {
   userid = req.params.id;
-  res.redirect(`/login`);
+
+  if (User.findById(req.params.id) === undefined) {
+    req.flash("message", "Your Email is Verified !!! Please Login Here!");
+    res.redirect(`/login`);
+  } else {
+    req.flash(
+      "message",
+      "Please enter the correct Email-ID or check the Verification URL again"
+    );
+    res.redirect("/signup");
+  }
 });
 
 module.exports = route;
